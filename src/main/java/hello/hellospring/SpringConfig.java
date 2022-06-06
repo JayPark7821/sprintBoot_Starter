2b6cdf3eb9c398904b005453bf887c0c9ff7c514
JayPark7821 <pjhyun1990@gmail.com>
2022-06-06 20:03:29 +0900

    메모리 -> jdbc로 변경 springConfig 파일에서 interface로 선언한 MemberRepository의 구현체를 JdbcMemberRepository로 바꿔줌

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,10 +1,15 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import javax.sql.DataSource;
 
 /***
  * 스프링이 올라올때 @Configuration을 읽어
@@ -13,6 +18,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -20,7 +32,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 
